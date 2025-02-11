f1ae9be73969e9cdbac60a86a90d5a45ba7dfc10
Raissa Vitória <123013855+raissabispo@users.noreply.github.com>
2025-02-11 09:32:54 -0300

    Add files via upload

---
+++ p.py
@@ -0,0 +1,4 @@
+peso = float(("Digite seu peso(kg): "))
+altura = float(("Digite sua atura(m): "))
+imc =  peso/ (altura ** 2)
+print("Seu IMC é: ", imc )
---
+++ r.r
@@ -0,0 +1,4 @@
+peso <- as.numeric(readline("Digite seu peso(kg): "))
+altura <- as.numeric(readline("Digite sua atura(m): "))
+imc <- peso/ (altura ^ 2)
+print(paste("Seu IMC é: ", imc ))
